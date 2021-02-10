36b7257b12b5719ad7102b6b754d4a6589cc2ac2
lenovo <abdusatarovfaruh3@gmail.com>
2021-02-10 17:57:41 +0300

    master ok

--- app/src/main/java/com/abdusatarov/alexeyandauto/MainActivity.java
+++ app/src/main/java/com/abdusatarov/alexeyandauto/MainActivity.java
@@ -2,7 +2,9 @@ package com.abdusatarov.alexeyandauto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rashid);
+        text=findViewById(R.id.rashod);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this,AutoActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
