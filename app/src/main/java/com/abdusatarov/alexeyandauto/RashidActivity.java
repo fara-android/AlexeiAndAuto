715c50a48a51de915e375a1ee82c7242c7acc26c
lenovo <kasimovrashid777@gmqil.com>
2021-02-10 17:33:24 +0600

    enter B(Autoactivity)

--- app/src/main/java/com/abdusatarov/alexeyandauto/RashidActivity.java
+++ app/src/main/java/com/abdusatarov/alexeyandauto/RashidActivity.java
@@ -2,13 +2,25 @@ package com.abdusatarov.alexeyandauto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RashidActivity extends AppCompatActivity {
-
+ TextView enter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rashid);
+        enter=findViewById(R.id.rashod);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent=new Intent(RashidActivity.this,AutoActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
